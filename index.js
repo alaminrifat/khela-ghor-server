eda7ae36e71a2a85490d30dc88634acb080af7b5
alaminrifat <alaminrifat71@gmail.com>
2023-05-18 04:04:19 +0600

    mongodb connection , addtoy post

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
-const express = require('express')
-const app = express()
-var cors = require('cors')
-const port = 5000
-app.use(cors())
-app.use(express.json())
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vhaictv.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const toyCollection = client.db('khelaGhor').collection('toys');
+
+
+    // add a toy
+    app.post('/addtoy', async (req, res) => {
+        const toy = req.body;
+        console.log(toy);
+        const result = await toyCollection.insertOne(toy);
+        res.send(result);
+    });
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Khela ghor Server is Running...')
